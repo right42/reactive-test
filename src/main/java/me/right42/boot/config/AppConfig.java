66087e082c8461008ab0cf1739dda22450d6999b
right42 <chjm452@naver.com>
2020-11-07 23:28:23 +0900

    threadpool

---
+++ src/main/java/me/right42/boot/config/AppConfig.java
@@ -0,0 +1,19 @@
+package me.right42.boot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor(){
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setMaxPoolSize(10);
+        threadPoolTaskExecutor.setCorePoolSize(10);
+        threadPoolTaskExecutor.setThreadNamePrefix("my-pool");
+        return threadPoolTaskExecutor;
+    }
+
+}
